85c2c25fcaad5189de9494e2643bfdfe6b8e4fcb
Jeremy Wade <jeremy.wade.10@gmail.com>
2015-12-01 23:06:24 +0530

    Added secureRandom uuid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201150303) do
+ActiveRecord::Schema.define(version: 20151201172930) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20151201150303) do
     t.string   "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "uuid"
   end
 
   create_table "users", force: :cascade do |t|
